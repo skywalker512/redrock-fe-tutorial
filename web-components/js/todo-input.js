599a182900ca61d959e49454db36bae1db685e27
RetroAstro <452461114@qq.com>
2019-03-22 20:13:47 +0800

    web components

--- web-components/js/todo-input.js
+++ web-components/js/todo-input.js
@@ -1,33 +1,32 @@
-const templateTodoInput = document.createElement('template');
+const templateTodoInput = document.createElement('template')
+
 templateTodoInput.innerHTML = `
     <form id="new-todo-form">
         <input id="new-todo" type="text" placeholder="What needs to be done?">
     </form>
-`;
+`
 
 class TodoInput extends HTMLElement {
     constructor() {
-        super();
-        // Do not use shadow DOM to avoid problems when testing with selenium
-        // this._root = this.attachShadow({ 'mode': 'open' });
+        super()
     }
 
     connectedCallback() {
-        console.log('TodoInput ADDED TO THE DOM');
-        this.appendChild(templateTodoInput.content.cloneNode(true));
-        this.$form = this.querySelector('form');
-        this.$input = this.querySelector('input');
+        console.log('TodoInput ADDED TO THE DOM')
+        this.appendChild(templateTodoInput.content.cloneNode(true))
+        this.$form = this.querySelector('form')
+        this.$input = this.querySelector('input')
         this.$form.addEventListener("submit", (e) => {
-            e.preventDefault();
-            if (!this.$input.value) return;
-            this.dispatchEvent(new CustomEvent('onSubmit', { detail: this.$input.value }));
-            this.$input.value = '';
-        });
+            e.preventDefault()
+            if (!this.$input.value) return
+            this.dispatchEvent(new CustomEvent('onSubmit', { detail: this.$input.value }))
+            this.$input.value = ''
+        })
     }
 
     disconnectedCallback() {
-        console.log('TodoInput REMOVED TO THE DOM');
+        console.log('TodoInput REMOVED TO THE DOM')
     }
 }
 
-window.customElements.define('todo-input', TodoInput);
+window.customElements.define('todo-input', TodoInput)
--- web-components/js/todo-item.js
+++ web-components/js/todo-item.js
@@ -1,75 +1,74 @@
-const templateTodoItem = document.createElement('template');
+const templateTodoItem = document.createElement('template')
+
 templateTodoItem.innerHTML = `
     <li class="item">
         <input type="checkbox">
         <label></label>
         <button class="destroy">x</button>
     </li>
-`;
+`
 
 class TodoItem extends HTMLElement {
     constructor() {
-        super();
-        // Do not use shadow DOM to avoid problems when testing with selenium
-        // this._root = this.attachShadow({ 'mode': 'open' });
-        this._checked = false;
-        this._text = '';
+        super()
+        this._checked = false
+        this._text = ''
     }
 
     connectedCallback() {
-        this.appendChild(templateTodoItem.content.cloneNode(true));
-        this.$item = this.querySelector('.item');
-        this.$removeButton = this.querySelector('.destroy');
-        this.$text = this.querySelector('label');
-        this.$checkbox = this.querySelector('input');
+        this.appendChild(templateTodoItem.content.cloneNode(true))
+        this.$item = this.querySelector('.item')
+        this.$removeButton = this.querySelector('.destroy')
+        this.$text = this.querySelector('label')
+        this.$checkbox = this.querySelector('input')
         this.$removeButton.addEventListener('click', (e) => {
-            e.preventDefault();
-            this.dispatchEvent(new CustomEvent('onRemove', { detail: this.index }));
-        });
+            e.preventDefault()
+            this.dispatchEvent(new CustomEvent('onRemove', { detail: this.index }))
+        })
         this.$checkbox.addEventListener('click', (e) => {
-            e.preventDefault();
-            this.dispatchEvent(new CustomEvent('onToggle', { detail: this.index }));
-        });
-        this._render();
+            e.preventDefault()
+            this.dispatchEvent(new CustomEvent('onToggle', { detail: this.index }))
+        })
+        this._render()
     }
 
     disconnectedCallback() { }
 
     static get observedAttributes() {
-        return ['text'];
+        return ['text']
     }
+
     attributeChangedCallback(name, oldValue, newValue) {
-        this._text = newValue;
+        this._text = newValue
     }
 
     set index(value) {
-        this._index = value;
+        this._index = value
     }
 
     get index() {
-        return this._index;
+        return this._index
     }
 
     set checked(value) {
-        this._checked = Boolean(value);
+        this._checked = value
     }
 
     get checked() {
-        return this.hasAttribute('checked');
+        return this.hasAttribute('checked')
     }
 
     _render() {
-        if (!this.$item) return;
-        this.$text.textContent = this._text;
+        if (!this.$item) return
+        this.$text.textContent = this._text
         if (this._checked) {
-            this.$item.classList.add('completed');
-            this.$checkbox.setAttribute('checked', '');
+            this.$item.classList.add('completed')
+            this.$checkbox.setAttribute('checked', '')
         } else {
-            this.$item.classList.remove('completed');
-            this.$checkbox.removeAttribute('checked');
+            this.$item.classList.remove('completed')
+            this.$checkbox.removeAttribute('checked')
         }
-
     }
 }
 
-window.customElements.define('todo-item', TodoItem);
+window.customElements.define('todo-item', TodoItem)
